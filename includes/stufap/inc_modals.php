49c3f9354045a550de54a3c7f440a3d5b95973d2
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-10-27 15:47:42 +0800

    Update inc_modals.php

--- includes/stufap/inc_modals.php
+++ includes/stufap/inc_modals.php
@@ -791,7 +791,6 @@
                     <div class="form-group">
                         <label>1st Semester</label>
                         <div class="form-row">
-                        
                             <div class="col">
                                 <div class="input-group"><div class="input-group-prepend"><span class="input-group-text icon-container"><i class='fas fa-male' style='font-size:20px; color:#1a5676'></i></span></div><input id="total_fhe_1st_male" name="total_fhe_1st_male" class="form-control" type="number" min="0" placeholder="0">
                                 </div>
@@ -807,8 +806,14 @@
                     <div class="form-group">
                         <label>2nd Semester</label>
                         <div class="form-row">
-                            <div class="col"><input id="total_fhe_2nd_male" name="total_fhe_2nd_male" class="form-control" type="number" min="0"></div>
-                            <div class="col"><input id="total_fhe_2nd_female" name="total_fhe_2nd_female" class="form-control" type="number" min="0"></div>
+                            <div class="col">
+                                <div class="input-group"><div class="input-group-prepend"><span class="input-group-text icon-container"><i class='fas fa-male' style='font-size:20px; color:#1a5676'></i></span></div><input id="total_fhe_2nd_male" name="total_fhe_2nd_male" class="form-control" type="number" min="0" placeholder="0">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class='input-group'><div class='input-group-prepend'><span class='input-group-text icon-container'><i class='fas fa-female' style='font-size:20px; color:#9a0694'></i></span></div><input id="total_fhe_2nd_female" name="total_fhe_2nd_female" class="form-control" type="number" min="0" placeholder="0">
+                                </div>
+                            </div>
                         </div>
                     </div>
 
