8582e85128f2b5df35a3738e9caf525d048d7dd9
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-10-27 15:36:44 +0800

    Update inc_modals.php

--- includes/stufap/inc_modals.php
+++ includes/stufap/inc_modals.php
@@ -795,9 +795,11 @@
                             <div class="col">
                             <div class="input-group"><div class="input-group-prepend"><span class="input-group-text icon-container"><i class='fas fa-male' style='font-size:20px; color:#1a5676'></i></span></div><input id="total_fhe_1st_male" name="total_fhe_1st_male" class="form-control" type="number" min="0" placeholder="0">
                             </div>
-                                
+                            <div class="col">
+                            <div class="input-group"><div class="input-group-prepend"><span class="input-group-text icon-container"><i class='fas fa-female' style='font-size:20px; color:#1a5676'></i></span></div><input id="total_fhe_1st_female" name="total_fhe_1st_female" class="form-control" type="number" min="0" placeholder="0">
+                            </div> 
                             </div>
-                            <div class="col"><input id="total_fhe_1st_female" name="total_fhe_1st_female" class="form-control" type="number" min="0"></div>
+                            <!-- <div class="col"><input id="total_fhe_1st_female" name="total_fhe_1st_female" class="form-control" type="number" min="0"></div> -->
                         </div>
                     </div>
 
