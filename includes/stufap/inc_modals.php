cec6d3beb7c9fede77d98409a6e593fb187e8f57
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-10-27 16:51:52 +0800

    Update inc_modals.php

--- includes/stufap/inc_modals.php
+++ includes/stufap/inc_modals.php
@@ -980,30 +980,18 @@
                     <hr>
                     <div class="form-group"><label>Total Grantees</label>
                         <div class="form-group"><label>1st Term</label>
+                        <div class="form-group">
                             <div class="form-row">
                                 <div class="col"><input id="total_tes_1st_male" name="total_tes_1st_male" class="form-control" type="number" min="0"></div>
                                 <div class="col"><input id="total_tes_1st_female" name="total_tes_1st_female" class="form-control" type="number" min="0"></div>
                             </div>
+                        </div>
+                        <div class="form-group">
                             <div class="form-row">
                                 <div class="col"><input id="total_tes_1st_male" name="total_tes_1st_male" class="form-control" type="number" min="0"></div>
                                 <div class="col"><input id="total_tes_1st_female" name="total_tes_1st_female" class="form-control" type="number" min="0"></div>
                             </div>
-                            <div class="form-row">
-                                <div class="col"><input id="total_tes_1st_male" name="total_tes_1st_male" class="form-control" type="number" min="0"></div>
-                                <div class="col"><input id="total_tes_1st_female" name="total_tes_1st_female" class="form-control" type="number" min="0"></div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col"><input id="total_tes_1st_male" name="total_tes_1st_male" class="form-control" type="number" min="0"></div>
-                                <div class="col"><input id="total_tes_1st_female" name="total_tes_1st_female" class="form-control" type="number" min="0"></div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col"><input id="total_tes_1st_male" name="total_tes_1st_male" class="form-control" type="number" min="0"></div>
-                                <div class="col"><input id="total_tes_1st_female" name="total_tes_1st_female" class="form-control" type="number" min="0"></div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col"><input id="total_tes_1st_male" name="total_tes_1st_male" class="form-control" type="number" min="0"></div>
-                                <div class="col"><input id="total_tes_1st_female" name="total_tes_1st_female" class="form-control" type="number" min="0"></div>
-                            </div>
+                        </div>
                         </div>
                         <div class="form-group"><label>2nd Term</label>
                             <div class="form-row">
