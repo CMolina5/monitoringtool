8934cd5b363e30caaf4acdaf6c3a74d00aba4100
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-11-02 13:48:35 +0800

    Update datatables.js

--- assets/js/datatables.js
+++ assets/js/datatables.js
@@ -125,7 +125,7 @@ $(document).ready(function () {
         "pageLength": 5,
         orderCellsTop: true,
         fixedHeader: true,
-        "deferRender": true,
+        // "deferRender": true,
         scrollX:        200,
         scrollCollapse: true,
         scroller:       true
