73f530491a0d1762decaea5e5e59a46a7159270e
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-11-15 06:55:38 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 include_once 'includes/db_connection.php';
+if (! empty($_SESSION["hei_uii"])) {
+    header("Location:../index.php");
+}
 header('Content-Type: text/html; charset="UTF-8"');
 ?>
 
