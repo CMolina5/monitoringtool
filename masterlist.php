dd0bb04ee28a797ad2b76faa17400da7426c5513
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-01 07:26:47 +0800

    Update masterlist.php

--- masterlist.php
+++ masterlist.php
@@ -165,9 +165,9 @@ $pdf->Ln();
 if ($_SESSION['hei_it'] == 'Private HEI') {
 $pdf->SetFont('Arial', 'B', 10);
 $pdf->Cell(95, 5, 'Private HEI located in city/municipality w/ no SUC/LUC?', 0, 0, 'L', true);
-}
 $pdf->SetFont('Arial', '', 9);
-$pdf->Cell(73, 5, 'N/A', 0, 0, 'L', true);
+$pdf->Cell(73, 5, $_SESSION['hei_pnsl'], 0, 0, 'L', true);
+}
 $pdf->SetFont('Arial', 'B', 10);
 $pdf->Cell(47, 5, 'Alternative Email Address:', 0, 0, 'L', true);
 $pdf->SetFont('Arial', '', 9);
