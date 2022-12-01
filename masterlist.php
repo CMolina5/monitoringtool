de4f745092302e85be887320700588feb53f48ef
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-01 17:17:03 +0800

    Update masterlist.php

--- masterlist.php
+++ masterlist.php
@@ -62,6 +62,15 @@ $pdf->AddPage();
 //Cell(width , height , text , border , end line , [align] )
 //table header
 //set font to arial, bold, 7pt
+
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->SetWidths(array(168, 168));
+$pdf->SetAligns(array('L', 'L'));
+$pdf->SetFillColor(255, 0, 0);
+$pdf->SetTextColor(255, 255, 255);
+$pdf->Cell(336, 8, 'WARNING: FOR REVIEWING PURPOSES ONLY! DO NOT PRINT THIS PAGE!', 0, 0, 'C', true);
+$pdf->Ln();
+
 $pdf->SetFont('Arial', 'B', 15);
 $pdf->SetTextColor(0, 0, 0);
 $pdf->SetFillColor(255, 255, 255);
