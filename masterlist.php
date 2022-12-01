07c92cd3c8a7bf0207784cd3c1bf6fc25e6b5bbe
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-01 08:52:14 +0800

    Update masterlist.php

--- masterlist.php
+++ masterlist.php
@@ -3370,7 +3370,7 @@ $pdf->Cell(95, 5, 'Date:', 0, 0, 'L', true);
 
 //end of data rows
 $pdf->Output('assets/pdf/'. $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . '.pdf', 'F');
-$pdf->Output( $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . '.pdf', 'D');
+$pdf->Output( $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . '.pdf', 'I');
 
 // header('Content-type: application/pdf');
 // header('Content-disposition: attachment; filename =' . $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . '.pdf');
