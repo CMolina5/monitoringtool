875465f9ed49df6e0823502d6f338fc628cde094
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-01 09:54:22 +0800

    Update final.php

--- final.php
+++ final.php
@@ -43,9 +43,9 @@
 
     <!--Academic Year, School Calendar and Programs-->
     <div class="contact-clean">
-        <form id="hei-final">
+        <form>
         <?php
-            echo "<iframe src='assets/pdf/". $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . ".pdf' height='150%' width='100%' title='Iframe Example'></iframe>"
+            echo "<iframe src='assets/pdf/". $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . ".pdf' frameborder='0' style='overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px' height='100%' width='100%'></iframe>"
         ?>
         </form>
     </div>
