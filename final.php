5ecb181b8c06aa95e135eadd25faaf6be7d450dd
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-01 09:57:45 +0800

    Update final.php

--- final.php
+++ final.php
@@ -45,7 +45,7 @@
     <div class="contact-clean">
         <form id="hei-final">
         <?php
-            echo "<iframe src='assets/pdf/". $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . ".pdf' frameborder='0' style='overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;' height='100%' width='100%'></iframe>"
+            echo "<iframe src='assets/pdf/". $_SESSION['hei_name'] . "-" . $_SESSION['ac_year'] . ".pdf' frameborder='0' style='overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;left:0px;right:0px;bottom:0px' height='100%' width='100%'></iframe>"
         ?>
         </form>
     </div>
