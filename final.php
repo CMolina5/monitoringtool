0abb2b6a5c3c075cdc480ea43da410218e4a716c
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-01 15:18:51 +0800

    Update final.php

--- final.php
+++ final.php
@@ -46,7 +46,7 @@
             <button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="nav navbar-nav ml-auto">
-                    <li role="presentation" class="nav-item mx-0 mx-lg-1"><a class="btn btn-primary btn-final" role="button" data-toggle="tooltip" type="button" title="Home"><i class="fas fa-home"></i></a></li>
+                    <li role="presentation" class="nav-item mx-0 mx-lg-1"><a href="home.php" class="btn btn-primary btn-final" role="button" data-toggle="tooltip" type="button" title="Home"><i class="fas fa-home"></i></a></li>
                     <li role="presentation" class="nav-item mx-0 mx-lg-1"><button class="btn btn-primary btn-final" data-toggle="tooltip" type="button" title="Edit"><i class="far fa-edit"></i></button></li>
                     <li role="presentation" class="nav-item mx-0 mx-lg-1"><button class="btn btn-primary btn-final" data-toggle="tooltip" type="button" title="Edit"><i class="far fa-save"></i></button></li>
                     <li role="presentation" class="nav-item mx-0 mx-lg-1"><button class="btn btn-primary btn-final" data-toggle="tooltip" type="button" title="Finalize"><i class="far fa-check-circle"></i></button></li>
