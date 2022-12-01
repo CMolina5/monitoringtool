84730d2740844eedd728dd4b5442a824930a33a3
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-01 16:18:18 +0800

    Update final.php

--- final.php
+++ final.php
@@ -48,7 +48,7 @@
                 <ul class="nav navbar-nav ml-auto">
                     <li role="presentation" class="nav-item mx-0 mx-lg-1"><a href="home.php" class="btn btn-primary btn-final" role="button" data-toggle="tooltip" type="button" title="Home"><i class="fas fa-home"></i></a></li>
                     <li role="presentation" class="nav-item mx-0 mx-lg-1"><a href="heiprofile.php" class="btn btn-primary btn-final" data-toggle="tooltip" type="button" title="Edit"><i class="far fa-edit"></i></a></li>
-                    <li role="presentation" class="nav-item mx-0 mx-lg-1"><button class="btn btn-primary btn-final" data-toggle="tooltip" type="button" title="Save"><i class="far fa-save"></i></button></li>
+                    <li role="presentation" class="nav-item mx-0 mx-lg-1"><a href="includes/final/save.php" class="btn btn-primary btn-final" data-toggle="tooltip" type="button" title="Save"><i class="far fa-save"></i></a></li>
                     <li role="presentation" class="nav-item mx-0 mx-lg-1"><button class="btn btn-primary btn-final" data-toggle="tooltip" type="button" title="Finalize"><i class="far fa-check-circle"></i></button></li>
                 </ul>
             </div>
