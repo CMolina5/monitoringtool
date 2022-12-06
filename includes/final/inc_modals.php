00605112611c8d3f7db312b62f13d204a68776d2
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-12-06 10:55:49 +0800

    Update inc_modals.php

--- includes/final/inc_modals.php
+++ includes/final/inc_modals.php
@@ -1,25 +1,26 @@
 <div role="dialog" tabindex="-1" class="modal fade show" id="notice_modal">
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
-            <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
-            <div class="modal-body">
-                <form>
+            <form method="POST" id="notice_modal_form">
+                <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
                     <div class="form-group text-center">
                         <h4 class="border-dark">DATA PRIVACY NOTICE</h4>
                     </div>
                     <div class="form-group text-center">
                         <div class="form-row">
                             <div class="col-xl-10 offset-xl-1">
-                                <p class="text-left">In submitting this monitoring tool, I agree to the details being collected for the purposes of monitoring and evaluation of the programs under Republic Act No. 10931. I understand that:<br /></p>
+                                <p class="text-left">In submitting this monitoring tool, I agree to the details being collected for the purposes of monitoring and evaluation of the programs under Republic Act No. 10931. I understand that:<br /></p>
                                 <p class="text-left">1. All collected data shall be used for intended use only;<br />2. The information will only be accessed by the UniFAST. All collected data will be held securely and will not be distributed to<br />unauthorized or third
                                     parties; and<br />3. When collected information is no longer required for the intended purposes, all data will be disposed of by the UniFAST<br />in accordance with Republic Act No. 10173, otherwise known as the Data
-                                    Privacy Act of 2012. <br /></p>
+                                    Privacy Act of 2012. <br /></p>
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div class="modal-footer"><button class="btn btn-light" type="button" data-dismiss="modal">Close</button><button class="btn btn-primary" type="button">I Agree</button></div>
+                </div>
+                <div class="modal-footer"><button class="btn btn-light" type="button" data-dismiss="modal">Close</button><button class="btn btn-primary" type="button">I Agree</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
