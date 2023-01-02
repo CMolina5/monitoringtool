5a70e1b22de30689477898cbac45f13a77b49455
Carlo Molina <cmolina.unifast@ched.gov.ph>
2023-01-03 07:49:08 +0800

    Update inc_hei_records_new.php

--- includes/home/inc_hei_records_new.php
+++ includes/home/inc_hei_records_new.php
@@ -42,7 +42,7 @@ $resultCheck= mysqli_num_rows($result);
                     <div class="col-9 col-sm-11 col-md-11 col-lg-11">
                         <span class="badge badge-success">ONGOING</span>
                     </div>
-                    <div class="col-3 col-sm-1 col-md-1 col-lg-1 col-xl-1 text-right"><a class="text-muted" href="#"><i class="far fa-edit"></i></a></div>
+                    <div class="col-3 col-sm-1 col-md-1 col-lg-1 col-xl-1 text-right"><a class="text-muted" href="#"><i class="far fa-edit" style="font-size: 12px;"></i></a></div>
                 </div>
                 <div class="form-row" style="height: 99px;">
                     <div class="col">
