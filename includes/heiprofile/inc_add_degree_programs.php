bd7a6fa061b21740178b2c110ffb84d10c9656ea
Carlo Molina <cmolina.unifast@ched.gov.ph>
2023-01-23 14:59:07 +0800

    Update inc_add_degree_programs.php

--- includes/heiprofile/inc_add_degree_programs.php
+++ includes/heiprofile/inc_add_degree_programs.php
@@ -12,9 +12,22 @@ $result = mysqli_query($conn, $sql);
 $resultCheck = mysqli_num_rows($result);
 
 if ($resultCheck > 0) {
-    $sql = "INSERT INTO tbl_degree_programs_temp (ac_year, hei_psg_region, hei_uii, hei_name, program_code, program_name, gr_no, copc_no, in_the_portal)
-        VALUES ('$_SESSION[ac_year]', '$_SESSION[hei_psg_region]', '$_SESSION[hei_uii]', '$_SESSION[hei_name]', '$program_code','$program_name','$gr_no','$copc_no', 'No')";
+    $sql = "SELECT * FROM tbl_degree_programs_temp WHERE hei_uii='$_SESSION[hei_uii]' AND ac_year='$_SESSION[ac_year]' AND program_name ='$program'";
     $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0) {
+        echo"<script>
+                Swal.fire(
+                    'Good job!',
+                    'You clicked the button!',
+                    'success'
+                )
+            </script>";
+    }else{
+        $sql = "INSERT INTO tbl_degree_programs_temp (ac_year, hei_psg_region, hei_uii, hei_name, program_code, program_name, gr_no, copc_no, in_the_portal)
+        VALUES ('$_SESSION[ac_year]', '$_SESSION[hei_psg_region]', '$_SESSION[hei_uii]', '$_SESSION[hei_name]', '$program_code','$program_name','$gr_no','$copc_no', 'No')";
+        $result = mysqli_query($conn, $sql);
+    }
 } else {
     $sql = "INSERT INTO tbl_degree_programs (ac_year, hei_psg_region, hei_uii, hei_name, program_code, program_name, gr_no, copc_no, in_the_portal)
     VALUES ('$_SESSION[ac_year]', '$_SESSION[hei_psg_region]', '$_SESSION[hei_uii]', '$_SESSION[hei_name]', '$program_code','$program_name','$gr_no','$copc_no', 'No')";
