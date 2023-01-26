afee610f05a24dbb737f4a60361f24e2016b51c4
Carlo Molina <cmolina.unifast@ched.gov.ph>
2023-01-26 10:33:24 +0800

    Update stufap.js

--- assets/js/stufap.js
+++ assets/js/stufap.js
@@ -139,7 +139,7 @@ $(document).ready(function () {
     //add fhe category to the table
     $('#add_fhe_category_modal').on('submit', function (event) {//modal id
         event.preventDefault();
-        if($('#fhe_category').val() === null){
+        if($('#fhe_category').val() === null || $('#fhe_category').val() === ""){
             // console.log($('#gr_no').val() + $('#copc_no').val());
             Swal.fire(
                 'You missed something!',
