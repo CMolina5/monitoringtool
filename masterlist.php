958649033fec08111d816128b3ef83b9380e4dc0
Carlo Molina <cmolina.unifast@ched.gov.ph>
2023-02-09 17:22:16 +0800

    Update masterlist.php

--- masterlist.php
+++ masterlist.php
@@ -871,10 +871,10 @@ if ($fhe == 'yes') {
 
     $sql = "SELECT *
 FROM tbl_degree_programs 
-WHERE hei_uii='$_SESSION[hei_uii]' AND ac_year='$_SESSION[ac_year]' AND  (total_fhe_1sem_1yr_male != 0 AND total_fhe_1sem_1yr_female != 0 AND total_fhe_1sem_2yr_male != 0 AND total_fhe_1sem_2yr_female != 0 AND total_fhe_1sem_3yr_male != 0 AND total_fhe_1sem_3yr_female != 0 AND total_fhe_1sem_4yr_male != 0 AND total_fhe_1sem_4yr_female != 0 AND total_fhe_1sem_5yr_male != 0 AND total_fhe_1sem_5yr_female != 0 AND total_fhe_1sem_6yr_male != 0 AND total_fhe_1sem_6yr_female != 0 AND 
-total_fhe_2sem_1yr_male != 0 AND total_fhe_2sem_1yr_female != 0 AND total_fhe_2sem_2yr_male != 0 AND total_fhe_2sem_2yr_female != 0 AND total_fhe_2sem_3yr_male != 0 AND total_fhe_2sem_3yr_female != 0 AND total_fhe_2sem_4yr_male != 0 AND total_fhe_2sem_4yr_female != 0 AND total_fhe_2sem_5yr_male != 0 AND total_fhe_2sem_5yr_female != 0 AND total_fhe_2sem_6yr_male != 0 AND total_fhe_2sem_6yr_female != 0 AND
-total_fhe_3sem_1yr_male != 0 AND total_fhe_3sem_1yr_female != 0 AND total_fhe_3sem_2yr_male != 0 AND total_fhe_3sem_2yr_female != 0 AND total_fhe_3sem_3yr_male != 0 AND total_fhe_3sem_3yr_female != 0 AND total_fhe_3sem_4yr_male != 0 AND total_fhe_3sem_4yr_female != 0 AND total_fhe_3sem_5yr_male != 0 AND total_fhe_3sem_5yr_female != 0 AND total_fhe_3sem_6yr_male != 0 AND total_fhe_3sem_6yr_female != 0 AND
-total_fhe_sum_mid_1yr_male != 0 AND total_fhe_sum_mid_1yr_female != 0 AND total_fhe_sum_mid_2yr_male != 0 AND total_fhe_sum_mid_2yr_female != 0 AND total_fhe_sum_mid_3yr_male != 0 AND total_fhe_sum_mid_3yr_female != 0 AND total_fhe_sum_mid_4yr_male != 0 AND total_fhe_sum_mid_4yr_female != 0 AND total_fhe_sum_mid_5yr_male != 0 AND total_fhe_sum_mid_5yr_female != 0 AND total_fhe_sum_mid_6yr_male != 0 AND total_fhe_sum_mid_6yr_female != 0)
+WHERE hei_uii='$_SESSION[hei_uii]' AND ac_year='$_SESSION[ac_year]' AND  (total_fhe_1sem_1yr_male != 0 OR total_fhe_1sem_1yr_female != 0 OR total_fhe_1sem_2yr_male != 0 OR total_fhe_1sem_2yr_female != 0 OR total_fhe_1sem_3yr_male != 0 OR total_fhe_1sem_3yr_female != 0 OR total_fhe_1sem_4yr_male != 0 OR total_fhe_1sem_4yr_female != 0 OR total_fhe_1sem_5yr_male != 0 OR total_fhe_1sem_5yr_female != 0 OR total_fhe_1sem_6yr_male != 0 OR total_fhe_1sem_6yr_female != 0 OR 
+total_fhe_2sem_1yr_male != 0 OR total_fhe_2sem_1yr_female != 0 OR total_fhe_2sem_2yr_male != 0 OR total_fhe_2sem_2yr_female != 0 OR total_fhe_2sem_3yr_male != 0 OR total_fhe_2sem_3yr_female != 0 OR total_fhe_2sem_4yr_male != 0 OR total_fhe_2sem_4yr_female != 0 OR total_fhe_2sem_5yr_male != 0 OR total_fhe_2sem_5yr_female != 0 OR total_fhe_2sem_6yr_male != 0 OR total_fhe_2sem_6yr_female != 0 OR
+total_fhe_3sem_1yr_male != 0 OR total_fhe_3sem_1yr_female != 0 OR total_fhe_3sem_2yr_male != 0 OR total_fhe_3sem_2yr_female != 0 OR total_fhe_3sem_3yr_male != 0 OR total_fhe_3sem_3yr_female != 0 OR total_fhe_3sem_4yr_male != 0 OR total_fhe_3sem_4yr_female != 0 OR total_fhe_3sem_5yr_male != 0 OR total_fhe_3sem_5yr_female != 0 OR total_fhe_3sem_6yr_male != 0 OR total_fhe_3sem_6yr_female != 0 OR
+total_fhe_sum_mid_1yr_male != 0 OR total_fhe_sum_mid_1yr_female != 0 OR total_fhe_sum_mid_2yr_male != 0 OR total_fhe_sum_mid_2yr_female != 0 OR total_fhe_sum_mid_3yr_male != 0 OR total_fhe_sum_mid_3yr_female != 0 OR total_fhe_sum_mid_4yr_male != 0 OR total_fhe_sum_mid_4yr_female != 0 OR total_fhe_sum_mid_5yr_male != 0 OR total_fhe_sum_mid_5yr_female != 0 OR total_fhe_sum_mid_6yr_male != 0 OR total_fhe_sum_mid_6yr_female != 0)
 
 ORDER BY program_name ASC";
 
@@ -2975,7 +2975,7 @@ WHERE hei_uii='$_SESSION[hei_uii]' AND ac_year='$_SESSION[ac_year]' AND (total_t
 
         //END
         $pdf->Ln();
-        $pdf->Ln();
+        $pdf->Ln(); 
     }
 
 
