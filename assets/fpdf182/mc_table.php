7545cbfd9984536649e44343ab9d1f4ba53c8c1f
Carlo Molina <cmolina.unifast@ched.gov.ph>
2023-02-16 09:57:41 +0800

    Update mc_table.php

--- assets/fpdf182/mc_table.php
+++ assets/fpdf182/mc_table.php
@@ -36,6 +36,34 @@ function Row($data)
 		$x=$this->GetX();
 		$y=$this->GetY();
 		//Draw the border
+		$this->Rect($x,$y,$w,$h);
+		//Print the text
+		$this->MultiCell($w,5,$data[$i],0,$a);
+		//Put the position to the right of the cell
+		$this->SetXY($x+$w,$y);
+	}
+	//Go to the next line
+	$this->Ln($h);
+}
+
+function RowNoBorder($data)
+{
+	//Calculate the height of the row
+	$nb=0;
+	for($i=0;$i<count($data);$i++)
+		$nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+	$h=5*$nb;
+	//Issue a page break first if needed
+	$this->CheckPageBreak($h);
+	//Draw the cells of the row
+	for($i=0;$i<count($data);$i++)
+	{
+		$w=$this->widths[$i];
+		$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+		//Save the current position
+		$x=$this->GetX();
+		$y=$this->GetY();
+		//Draw the border
 		// $this->Rect($x,$y,$w,$h);
 		//Print the text
 		$this->MultiCell($w,5,$data[$i],0,$a);
