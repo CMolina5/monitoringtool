b4312d0ecc75443fc38bb021b84003d8dba3face
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-08-07 11:52:18 +0800

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbServername = "unifast.gov.ph";
-$dbUsername = "unifastgov_monitoring";
-$dbPassword = "R@10931!!!";
+$dbUsername = "unifastgov_monitoringtool";
+$dbPassword = "VF&a19jNGJ&Y";
 $dbName = "unifastgov_ufdb";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
